84f92b5d882776115b15eec0fe9fd5c2ecc6e02d
a-lvaro <aaflneto@gmail.com>
2023-06-02 13:00:34 -0300

    sugestões aceitas

--- app/models/validator/usuario.py
+++ app/models/validator/usuario.py
@@ -1,25 +1,34 @@
 from pymongo import MongoClient
 from cerberus import Validator
 
-class ValidarUsuario():
+
+class ValidarUsuario:
     def __init__(self):
         cliente = MongoClient()
-        db = cliente['petBD']
-        self.__colecao = db['usuarios']
+        db = cliente["petBD"]
+        self.__colecao = db["usuarios"]
 
-    def Usuario(self):
-        self.__colecao.create_index('cpf', unique=True)
-        self.__colecao.create_index('email', unique=True)
+    def usuario(self) -> Validator:
+        self.__colecao.create_index("cpf", unique=True)
+        self.__colecao.create_index("email", unique=True)
 
         schemaUsuarios = {
-            'nome': {'type': 'string', 'required': True},
-            'email': {'type': 'string', 'required': True},
-            'cpf': {'type': 'string', 'required': True},
-            'curso': {'type': 'string', 'required': True},
-            'estado': {'type': 'string', 'required': True, 'allowed': ['ativo', 'inativo']},
-            'senha': {'type': 'string', 'required': True, 'minlength': 8},
-            'petiano': {'type': 'string', 'required': True, 'allowed': ['sim', 'nao', 'egresso']},
-            'data_criacao': {'type': 'datetime'}
+            "nome": {"type": "string", "required": True},
+            "email": {"type": "string", "required": True},
+            "cpf": {"type": "string", "required": True},
+            "curso": {"type": "string", "required": True},
+            "estado": {
+                "type": "string",
+                "required": True,
+                "allowed": ["ativo", "inativo"],
+            },
+            "senha": {"type": "string", "required": True},
+            "petiano": {
+                "type": "string",
+                "required": True,
+                "allowed": ["sim", "nao", "egresso"],
+            },
+            "data_criacao": {"type": "datetime"},
         }
 
         validadorUsuarios = Validator(schemaUsuarios)
