aa47c73fde219c9282324a6148a9dff0e08af75a
pinheiroo <lucaspin03@hotmail.com>
2023-06-02 12:15:33 -0300

    formatando

--- app/controllers/OperacoesEmail.py
+++ app/controllers/OperacoesEmail.py
@@ -1,17 +1,34 @@
-import ssl,smtplib
+import ssl, smtplib
 from email.message import EmailMessage
 
-#Função que envia email para avisar sobre inscrição do evento 
-def emailConfirmacaoEvento(emailPet: str, senhaPet: str, emailDestino: str, nomeEvento: str, localEvento: str, dataEvento: str, coindicoesEvento: str) -> None:
+
+# Função que envia email para avisar sobre inscrição do evento
+def emailConfirmacaoEvento(
+    emailPet: str,
+    senhaPet: str,
+    emailDestino: str,
+    nomeEvento: str,
+    localEvento: str,
+    dataEvento: str,
+    coindicoesEvento: str,
+) -> None:
     m: EmailMessage = EmailMessage()
-    m['From'] = emailPet
-    m['To'] = emailDestino
-    m['Subject'] = "PET-Info: Você foi cadastrado no evento" + nomeEvento
-    m.set_content("Nome do evento: " + nomeEvento + "\nLocal do Evento: " + localEvento + "\nData do evento: " + dataEvento + "Nesse evento você optou por: " + coindicoesEvento)
+    m["From"] = emailPet
+    m["To"] = emailDestino
+    m["Subject"] = "PET-Info: Você foi cadastrado no evento" + nomeEvento
+    m.set_content(
+        "Nome do evento: "
+        + nomeEvento
+        + "\nLocal do Evento: "
+        + localEvento
+        + "\nData do evento: "
+        + dataEvento
+        + "Nesse evento você optou por: "
+        + coindicoesEvento
+    )
 
     contexto: ssl.SSLContext = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=contexto) as smtp:
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=contexto) as smtp:
         smtp.login(emailPet, senhaPet)
         smtp.sendmail(emailPet, emailDestino, m.as_string)
-
