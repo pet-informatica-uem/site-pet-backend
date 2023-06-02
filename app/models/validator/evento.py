ab5d0c078260054360cb33961dca1cdb9720f199
a-lvaro <aaflneto@gmail.com>
2023-06-02 12:31:29 -0300

    principais validadores do banco

---
+++ app/models/validator/evento.py
@@ -0,0 +1,60 @@
+from pymongo import MongoClient
+from cerberus import Validator
+
+class ValidarEvento():
+    def __init__(self):
+        cliente = MongoClient()
+        db = cliente['petBD']
+        self.__colecao = db['eventos']
+
+    def evento(self):
+        self.__colecao.create_index('nome evento', unique=True)
+
+        schemaEvento = {
+            'nome evento': {'type': 'string', 'required': True},  
+            'resumo': {'type': 'string', 'required': True},
+            'pré-requisitos': {'type': 'string', 'required': True},
+            'data criação': {'type': 'datetime', 'required': True},
+            'data evento': {'type': 'datetime', 'required': True},
+            'hora evento': {'type': 'string', 'required': True},
+            'data inicio inscrição': {'type': 'datetime', 'required': True},
+            'data fim inscrição': {'type': 'datetime', 'required': True},
+            'local': {'type': 'string', 'required': True},
+            'total vagas ofertadas': {'type': 'string', 'required': True},
+            'laboratorio': {'type': 'string', 'required': True, 'allowed': ['sim', 'nao']},
+            'vagas com notebook': {'type': 'string', 'required': True},
+            'vagas sem notebook': {'type': 'string', 'required': True},
+            'carga horária': {'type': 'string', 'required': True},
+            'pago': {'type': 'string', 'required': True, 'allowed': ['sim', 'nao']},
+            'valor': {'type': 'string', 'required': False},
+            'arte evento': {'type': 'binary', 'required': True},
+            'qr code': {'type': 'string', 'required': True, 'allowed': ['sim', 'nao']},
+            'arte qrcode': {'type': 'binary', 'required': False},
+            'inscritos': {
+                'type': 'list', 
+                'required': False, 
+                'schema': {
+                    'type': 'dict',
+                    'schema': {
+                        'idUsuario': {'type': 'objectid'},
+                        'data': {'type': 'datetime'},
+                        'hora': {'type': 'string'},
+                    },
+                },
+            },
+            'presentes': {
+                'type': 'list', 
+                'required': False, 
+                'schema': {
+                    'type': 'dict',
+                    'schema': {
+                        'idUsuario': {'type': 'objectid'},
+                        'data': {'type': 'datetime'},
+                        'hora': {'type': 'string'},
+                    },
+                },
+            },
+        }
+
+        validadorEvento = Validator(schemaEvento)
+        return validadorEvento
