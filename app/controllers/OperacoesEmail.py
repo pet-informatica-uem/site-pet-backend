01429a8f54c7b1d73fc1890300a124bc5ecb03b3
pinheiroo <lucaspin03@hotmail.com>
2023-06-04 00:36:39 -0300

    alteracoes feitas

--- app/controllers/OperacoesEmail.py
+++ app/controllers/OperacoesEmail.py
@@ -1,49 +1,50 @@
 import ssl, smtplib
 from email.message import EmailMessage
 
-
+ 
 # Função para enviar email customizado
-def enviarEmail(
+def emailGenerico(
     emailPet: str, senhaPet: str, emailDestino: str, titulo: str, texto: str
 ) -> None:
-    m: EmailMessage = EmailMessage()
-    m["From"] = emailPet
-    m["To"] = emailDestino
-    m["Subject"] = titulo
-    m.set_content(texto)
+    mensagem: EmailMessage = EmailMessage()
+    mensagem["From"] = emailPet
+    mensagem["To"] = emailDestino
+    mensagem["Subject"] = titulo
+    mensagem.set_content(texto)
 
-    contexto: ssl.SSLContext = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=contexto) as smtp:
-        smtp.login(emailPet, senhaPet)
-        smtp.sendmail(emailPet, emailDestino, m.as_string())
+    enviarEmail(emailPet, senhaPet, emailDestino, mensagem)
 
 
 # Função para enviar verificação de email
-def verificaEmail(emailPet: str, senhaPet: str, emailDestino: str, codigo: int) -> None:
-    m: EmailMessage = EmailMessage()
-    m["From"] = emailPet
-    m["To"] = emailDestino
-    m["Subject"] = "IDPetInfo - Código de Verficação"
-    m.set_content("Seu código de verificação é: " + codigo)
+def verificarEmail(
+    emailPet: str, senhaPet: str, emailDestino: str, codigo: str
+) -> None:
+    mensagem: EmailMessage = EmailMessage()
+    mensagem["From"] = emailPet
+    mensagem["To"] = emailDestino
+    mensagem["Subject"] = "Pet-Info - Código de Verficação"
+    mensagem.set_content("Seu código de verificação é: " + codigo)
 
-    contexto: ssl.SSLContext = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=contexto) as smtp:
-        smtp.login(emailPet, senhaPet)
-        smtp.sendmail(emailPet, emailDestino, m.as_string)
+    enviarEmail(emailPet, senhaPet, emailDestino, mensagem)
 
 
 # Função para enviar link troca de senha
 def resetarSenha(emailPet: str, senhaPet: str, emailDestino: str, link: str) -> None:
-    m: EmailMessage = EmailMessage()
-    m["From"] = emailPet
-    m["To"] = emailDestino
-    m["Subject"] = "Site PET-Info - Reset de senha"
-    m.set_content("Para resetar sua senha, acesse o link: " + link)
+    mensagem: EmailMessage = EmailMessage()
+    mensagem["From"] = emailPet
+    mensagem["To"] = emailDestino
+    mensagem["Subject"] = "PET-Info - Reset de senha"
+    mensagem.set_content("Para resetar sua senha, acesse o link: " + link)
 
-    contexto: ssl.SSLContext = ssl.create_default_context()
+    enviarEmail(emailPet, senhaPet, emailDestino, mensagem)
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=contexto) as smtp:
-        smtp.login(emailPet, senhaPet)
-        smtp.sendmail(emailPet, emailDestino, m.as_string)
+
+def enviarEmail(emailPet: str, senhaPet: str, emailDestino: str, mensagem: str) -> None:
+    try:
+        contexto: ssl.SSLContext = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=contexto) as smtp:
+            smtp.login(emailPet, senhaPet)
+            smtp.sendmail(emailPet, emailDestino, mensagem.as_string())
+    except:
+        print("Erro! Falha ao enviar email")
