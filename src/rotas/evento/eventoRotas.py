76d1a6d2605676a7ce7ef62a9116625afc6625ce
JPAlkamim <jpalkamim@gmail.com>
2023-08-09 12:26:05 -0300

    ta com erro

--- src/rotas/evento/eventoRotas.py
+++ src/rotas/evento/eventoRotas.py
@@ -154,8 +154,11 @@ def getDadosInscricaoEvento(
     pagamento: Annotated[bool, Form()],
     nivelConhecimento: Annotated[str | None, Form(max_length=200)] = None,
 ):  
+    
+    print(idUsuario)
+    print(idEvento)
     inscritosController = InscritosEventoControlador()
-    situacaoInscricao :bool = inscritosController.inscricaoEventoControlador(idUsuario, idEvento, nivelConhecimento, tipoDeInscricao, pagamento)
+    situacaoInscricao :bool = inscritosController.inscricaoEventoControlador(idUsuario['id'], idEvento, nivelConhecimento, tipoDeInscricao, pagamento)
 
     #if not situacaoInscricao:
     #    raise alguma excecao kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk coringuei
