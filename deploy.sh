f3408a348708e407a892793c0da7558375a7beff
Lucas Wolschick <6264382+LucasWolschick@users.noreply.github.com>
2024-05-19 17:58:54 -0300

    O deploy funciona :)
    Só felicidade

--- deploy.sh
+++ deploy.sh
@@ -3,21 +3,21 @@
 REPO_DIR="/home/pet/site-pet/backend"
 SERVICE_NAME="site-pet-backend.service"
 
-cd $REPO_DIR
+cd $REPO_DIR || exit 1
 
-git fetch
+git fetch origin
 
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse @{u})
 
 if [ $LOCAL != $REMOTE ]; then
-    systemctl stop $SERVICE_NAME
+    sudo /bin/systemctl stop $SERVICE_NAME
     
-    if git pull --rebase origin main; then
-        systemctl start $SERVICE_NAME
+    if git pull --rebase origin stable; then
+        sudo /bin/systemctl start $SERVICE_NAME
     else
         echo "Error: failed to pull changes; aborting update"
         git rebase --abort
-        systemctl start $SERVICE_NAME
+        sudo /bin/systemctl start $SERVICE_NAME
     fi
 fi
