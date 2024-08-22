f653c59f4b7941826194fdf00f045253a1f9b72d
Ana Paula <164682309+anapaulacrippa@users.noreply.github.com>
2024-08-22 17:08:45 -0300

    documentacao eventoQuery.py

--- src/modelos/evento/eventoQuery.py
+++ src/modelos/evento/eventoQuery.py
@@ -2,6 +2,10 @@ from enum import Enum
 
 
 class eventoQuery(str, Enum):
+    """
+    Classe que utiliza tipos enumerados para representar o estado de um evento.
+    """
+
     PASSADO = "passados"
     PRESENTE = "atuais"
     FUTURO = "futuros"
