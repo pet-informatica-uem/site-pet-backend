3b755978dcbafa4dfd9d46b6d9d45334116415f3
Ana Paula <164682309+anapaulacrippa@users.noreply.github.com>
2024-08-22 17:05:53 -0300

    documentacao eventoClad

--- src/modelos/evento/eventoClad.py
+++ src/modelos/evento/eventoClad.py
@@ -7,32 +7,52 @@ from src.modelos.evento.evento import Evento
 
 
 class EventoCriar(BaseModel):
+    """
+    Classe que representa um evento do sistema.
+    """
+
     titulo: str
+    "Título do evento."
 
     descricao: str
+    "Descrição do evento."
 
     preRequisitos: list[str] = []
+    "Pré requisitos para participar do evento."
 
     inicioInscricao: datetime
+    "Data e hora de início das inscrições."
 
     fimInscricao: datetime
+    "Data e hora de fim das inscrições."
 
     dias: list[tuple[datetime, datetime]]
+    "Lista de tuplas de data e hora de início e fim de cada dia do evento."
 
     local: str
+    "Local do evento."
 
     vagasComNote: int
+    "Quantidade de vagas com notebook."
 
     vagasSemNote: int
+    "Quantidade de vagas sem notebook."
 
     cargaHoraria: int
+    "Carga horária do evento."
 
     valor: float
+    "Valor da inscrição."
 
     @field_validator("dias")
     def diasValidos(
         cls, dias: list[tuple[datetime, datetime]]
     ) -> list[tuple[datetime, datetime]]:
+        """
+        Função para verificar se as datas de início e fim de cada dia do evento são válidas.
+            :cls: EventoCriar -> referência à classe EventoCriar, na qual o método está sendo definido.
+            :dias: list[tuple[datetime, datetime]] -> a data e a hora de início e fim de cada dia do evento.
+        """
         for i, dia in enumerate(dias):
             if dia[0] > dia[1]:
                 raise ValueError(
@@ -43,6 +63,10 @@ class EventoCriar(BaseModel):
 
     @model_validator(mode="after")
     def inscricoesValidas(self) -> Self:
+        """
+        Função para verificar se as datas das inscrições do evento são válidas.
+            :self: EventoCriar -> referência à classe EventoCriar, na qual o método está sendo definido.
+        """
         if self.inicioInscricao > self.fimInscricao:
             raise ValueError(
                 "A data de início das inscrições deve ser anterior à data de fim das inscrições."
@@ -56,32 +80,52 @@ class EventoLer(Evento):
 
 
 class EventoAtualizar(BaseModel):
+    """
+    Classe que representa a atualização de um evento do sistema.
+    """
+    
     titulo: str | None = None
+    "Título do evento."
 
     descricao: str | None = None
+    "Descrição do evento."
 
     preRequisitos: list[str] | None = None
+    "Pré requisitos para participar do evento."
 
     inicioInscricao: datetime | None = None
+    "Data e hora de início das inscrições."
 
     fimInscricao: datetime | None = None
+    "Data e hora de fim das inscrições."
 
     dias: list[tuple[datetime, datetime]] | None = None
+    "Lista de tuplas de data e hora de início e fim de cada dia do evento."
 
     local: str | None = None
+    "Local do evento."
 
     vagasComNote: int | None = None
+    "Quantidade de vagas com notebook."
 
     vagasSemNote: int | None = None
+    "Quantidade de vagas sem notebook."
 
     cargaHoraria: int | None = None
+    "Carga horária do evento."
 
     valor: float | None = None
+    "Valor da inscrição."
 
     @field_validator("dias")
     def diasValidos(
         cls, dias: list[tuple[datetime, datetime]] | None
     ) -> list[tuple[datetime, datetime]] | None:
+        """
+        Função para verificar se as datas a serem atualizar de início e fim de cada dia do evento são válidas.
+            :cls: EventoCriar -> referência à classe EventoCriar, na qual o método está sendo definido.
+            :dias: list[tuple[datetime, datetime]] -> a data e a hora de início e fim de cada dia do evento.
+        """
         if dias:
             for i, dia in enumerate(dias):
                 if dia[0] > dia[1]:
@@ -93,6 +137,10 @@ class EventoAtualizar(BaseModel):
 
     @model_validator(mode="after")
     def inscricoesValidas(self) -> Self:
+        """
+        Função para verificar se as datas a serem atualizadas das inscrições do evento são válidas.
+            :self: EventoCriar -> referência à classe EventoCriar, na qual o método está sendo definido.
+        """
         if (
             self.inicioInscricao
             and self.fimInscricao
